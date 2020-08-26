ea0072617cbcfa9ab587293c2aa7e9ffce464ff4
April Adams <illusionlace@gmail.com>
2020-08-26 14:57:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,8 +9,8 @@ def line(katz_deli)
 end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  spot = katz_deli.index(name)+1
-  puts "Welcome #{name}. You are number #{spot} in line."
+  space = katz_deli.index(name)+1
+  puts "Welcome, #{name}. You are number #{place} in line."
 end
 def now_serving(katz_deli)
   if katz_deli.empty?
