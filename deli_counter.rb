463b43fd15873159bfc501975481deac9c006d40
April Adams <illusionlace@gmail.com>
2020-08-26 15:27:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   else
     current_line = "The line is currently:"
     katz_deli.each.with_index(1) do |name, index|
-      current_line << "#{index}. #{name}"
+      current_line << " #{index}. #{name}"
     end
     puts current_line
   end
