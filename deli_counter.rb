629670415ff75c8b667c8a43ca44d6a7434ccfb6
April Adams <illusionlace@gmail.com>
2020-08-26 15:11:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts  "The line is currently empty."
   else
-    puts "The line is currently: 1. #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]} 4. #{katz_deli[3]}"
+    puts "The line is currently: 1.#{katz_deli[0]} 2.#{katz_deli[1]} 3.#{katz_deli[2]}"
   end
 end
 def take_a_number(katz_deli, name)
