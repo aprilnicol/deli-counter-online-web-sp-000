fcd6aeb54e7efd47ebb9fc20910cfc0abffb9790
April Adams <illusionlace@gmail.com>
2020-08-26 15:25:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,14 +3,14 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  else 
+  else
     current_line = "The line is currently:"
     katz_deli.each_with_index(1) do |name, index|
       current_line << "#{index}. #{name}"
-    end 
+    end
     puts current_line
-  end 
-end 
+  end
+end
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   place = katz_deli.index(name)+1
