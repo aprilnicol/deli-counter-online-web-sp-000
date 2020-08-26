4257e26e73042693e440e3d3dedcd6c6bc71b6c9
April Adams <illusionlace@gmail.com>
2020-08-26 15:21:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.count > 1
     print "The line is currently:"
-    katz_deli.each_with_index{|name, index| print "#{index +1}. #{name}"}
+    katz_deli.each_with_index{|name, index| puts "#{index +1}. #{name}"}
   else
     puts "The line is currently empty."
   end
