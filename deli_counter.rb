db4c46fbfa48f84f1cc0c592f52a00923f8bf82d
April Adams <illusionlace@gmail.com>
2020-08-26 14:49:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,4 +13,10 @@ def take_a_number(katz_deli, name)
   puts "Welcome #{name}. You are number #{place} in line."
 end
 def now_serving(katz_deli)
-  if 
+  if katz_deli == 0
+    puts "There is nobody waiting to be served!"
+  else 
+    the_next = katz_deli.shift 
+    puts "Currently serving #{the_next}"
+end 
+end 
