1aa491daa28f9d1cbcedb62f92a2560757195f87
April Adams <illusionlace@gmail.com>
2020-08-26 14:45:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  if katz_deli.empty?
+    puts  "The line is currently empty."
+  else
+    puts "The line is currently: 1 #{katz_deli[0]} 2. #{katz_deli[1]} 3. #{katz_deli[2]}"
+  end
+end
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  place = katz_deli.index(name)+1
+  puts "Welcome #{name}. You are number #{place} in line."
+end
+def now_serving(katz_deli)
+  if 
